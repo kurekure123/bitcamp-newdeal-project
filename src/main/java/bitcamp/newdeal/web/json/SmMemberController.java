b57e887d1ba7cc0172462526677ec1bdb1f87c3a
ljylhy3391 <ljylhy3391@naver.com>
2018-09-03 17:55:42 +0900

    오후 5:55 2018-09-03

--- src/main/java/bitcamp/newdeal/web/json/SmMemberController.java
+++ src/main/java/bitcamp/newdeal/web/json/SmMemberController.java
@@ -2,6 +2,9 @@ package bitcamp.newdeal.web.json;
 
 import java.util.HashMap;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,22 +16,57 @@ import bitcamp.newdeal.service.MemberService;
 @RestController
 @RequestMapping("/member")
 public class SmMemberController {
-    
+
     @Autowired
     MemberService memberService;
-    
+
     @PostMapping("signUp")
-    public Object signUp (Member member) {
-        
+    public Object signUp(Member member) {
+
         HashMap<String, Object> result = new HashMap<>();
         try {
-            memberService.add(member); 
+            memberService.add(member);
             result.put("status", "success");
         } catch (Exception e) {
             result.put("status", "fail");
             result.put("message", e.getMessage());
         }
-        
+
         return result;
     }
+
+    // 로그인 기능 구현 by 이성민
+    @PostMapping("signIn")
+    public Object signIn(String email, String password, HttpSession session) {
+
+        HashMap<String, Object> result = new HashMap<>();
+        try {
+            Member loginUser = memberService.getMember(email, password);
+            if (loginUser == null)
+                throw new Exception("로그인 실패!");
+
+            session.setAttribute("loginUser", loginUser);
+            /*
+             * Member member = (Member) session.getAttribute("loginUser");
+             * System.out.println(member);
+             */
+
+            result.put("loginUser", loginUser);
+            result.put("status", "success");
+
+        } catch (Exception e) {
+            result.put("status", "fail");
+            result.put("message", e.getMessage());
+        }
+        return result;
+    }
+
+    @PostMapping("logout")
+    public Object logout(HttpServletRequest request, HttpSession session) throws Exception {
+
+        System.out.println("로그아웃!");
+        session.invalidate();
+        return "success";
+    }
+
 }
